97dfb8b01139c383aa934990db97a55cb6e396e7
Armando Pereira <armando2019ti@gmail.com>
2022-11-07 11:35:36 -0300

    Projeto app

---
+++ keys.md
@@ -0,0 +1,6 @@
+353987763599-95l3jfe8906jr2tdd7g8f8fa78rvm2lf.apps.googleusercontent.com
+Seu ID de cliente	
+
+
+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
+Sua chave secreta de cliente
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,28 @@
+import Logo from "../assets/logo.svg";
+import { useAuth } from "../hooks/useAuth";
+import { Fontisto } from "@expo/vector-icons";
+import { Button } from "../components/Button";
+import { Center, Icon, Text } from "native-base";
+
+export function SignIn() {
+  const { signIn, isUserLoading } = useAuth();
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        mt={12}
+        type="SECONDARY"
+        title="Entrar com Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: "white" } }}
+      />
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+        de sua conta.
+      </Text>
+    </Center>
+  );
+}
